75a03232af9e34b2b34237aa8184a9b963107803
John Hobbs <john@velvetcache.org>
2026-08-20 17:34:09 -0500

    Big, ideally exhaustive parse test

--- parse/parse_test.go
+++ parse/parse_test.go
@@ -10,6 +10,91 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func Test_Parse_Exhaustive(t *testing.T) {
+	doc := `#define true 1
+#define name "CfgModule"
+#define value 42.01
+#define arr {1, "two", 3.01}
+
+class CfgBase;
+
+class CfgModule : CfgBase {
+	scope = 2;
+	model = "\dz\path\to\thing.p3d";
+	emptyArray[] = {};
+	nestedArray[] = {1, {2, 3}, 4};
+};
+
+// this is a nested class
+class CfgAnother {
+	myVar = 420;
+	class CfgInner {
+		scope = 2;
+	};
+};
+`
+
+	f, err := parse.Parse([]byte(doc), true)
+	require.NoError(t, err)
+
+	t.Log(f.String())
+	assert.Equal(
+		t,
+		&ast.File{
+			Directives: []ast.Node{
+				ast.Define{Identifier: "true", Value: ast.Integer(1)},
+				ast.Define{Identifier: "name", Value: ast.String("CfgModule")},
+				ast.Define{Identifier: "value", Value: ast.Float(42.01)},
+				ast.Define{
+					Identifier: "arr",
+					Value: ast.Array{
+						ast.Integer(1),
+						ast.String("two"),
+						ast.Float(3.01),
+					},
+				},
+			},
+			Declarations: []ast.Node{
+				ast.Class{Identifier: "CfgBase"},
+				ast.Class{
+					Identifier: "CfgModule",
+					Parent:     "CfgBase",
+					Body: ast.Block{
+						ast.Assignment{Identifier: "scope", Value: ast.Integer(2)},
+						ast.Assignment{Identifier: "model", Value: ast.String(`\dz\path\to\thing.p3d`)},
+						ast.Assignment{Identifier: "emptyArray", Value: ast.Array{}},
+						ast.Assignment{
+							Identifier: "nestedArray",
+							Value: ast.Array{
+								ast.Integer(1),
+								ast.Array{
+									ast.Integer(2),
+									ast.Integer(3),
+								},
+								ast.Integer(4),
+							},
+						},
+					},
+				},
+				ast.Comment(" this is a nested class"),
+				ast.Class{
+					Identifier: "CfgAnother",
+					Body: ast.Block{
+						ast.Assignment{Identifier: "myVar", Value: ast.Integer(420)},
+						ast.Class{
+							Identifier: "CfgInner",
+							Body: ast.Block{
+								ast.Assignment{Identifier: "scope", Value: ast.Integer(2)},
+							},
+						},
+					},
+				},
+			},
+		},
+		f,
+	)
+}
+
 func Test_Parse_Define(t *testing.T) {
 	p, err := parse.Parse([]byte(`#define true 1`), true)
 	require.NoError(t, err)
